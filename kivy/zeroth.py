cbb1475e42b34981111763b675d71b9f17041d7c
Heerdyes Mahapatro <heerdyes.mahapatro@gmail.com>
2020-09-03 01:18:16 +0530

    zeroth kivy program on rpi

---
+++ kivy/zeroth.py
@@ -0,0 +1,33 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.graphics import *
+from math import *
+
+
+class GenPaintWidget(Widget):
+  # weird RPi specific ghost click workaround
+  altsensor=False
+
+  def on_touch_down(self,touch):
+    GenPaintWidget.altsensor=not GenPaintWidget.altsensor
+    if GenPaintWidget.altsensor:
+      return
+    print('touched at (%d,%d)'%(touch.x,touch.y))
+    with self.canvas:
+      Color(0.2,0.8,0.7,0.8)
+      d1=30.
+      d2=30.
+      cx=touch.x
+      cy=touch.y
+      for i in range(180):
+        t=i*pi/180
+        Line(points=[cx+i,cy,cx+i,cy-d2*sin(4*t)],width=1)
+
+
+class GenPaintApp(App):
+  def build(self):
+    return GenPaintWidget()
+
+
+if __name__ == '__main__':
+  GenPaintApp().run()
