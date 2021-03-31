64a5ff0b5eb6d47ff43eab12058018231d4c7c1a
Heerdyes Mahapatro <heerdyes.mahapatro@gmail.com>
2021-03-31 20:34:19 +0530

    moving genart to its own function

--- framebuffer/fbzero.c
+++ framebuffer/fbzero.c
@@ -14,7 +14,8 @@
 const double PI=3.141592654;
 
 // the rgb color structure
-struct rgb{
+struct rgb
+{
   unsigned char r;
   unsigned char g;
   unsigned char b;
@@ -23,7 +24,8 @@ struct rgb{
 typedef struct rgb RGBCOLOR;
 
 // the color constructor
-RGBCOLOR* new_color(unsigned char _r,unsigned char _g,unsigned char _b){
+RGBCOLOR* new_color(unsigned char _r,unsigned char _g,unsigned char _b)
+{
   RGBCOLOR* color=malloc(sizeof(RGBCOLOR));
   color->r=_r;
   color->g=_g;
@@ -32,7 +34,8 @@ RGBCOLOR* new_color(unsigned char _r,unsigned char _g,unsigned char _b){
 }
 
 // function to compute the rgb integer from separate r,g,b bytes
-int compute_rgb(RGBCOLOR color){
+int compute_rgb(RGBCOLOR color)
+{
   // r * 2^16
   // g * 2^8
   // b * 2^0
@@ -47,15 +50,40 @@ int compute_rgb(RGBCOLOR color){
 }
 
 // the basis of all geometry: the point
-void dot(struct fb_var_screeninfo sinfo,uint32_t *fb,int x,int y,RGBCOLOR color){
+void dot(struct fb_var_screeninfo sinfo,uint32_t *fb,int x,int y,RGBCOLOR color)
+{
   // compute rgb int
   int rgbnum=compute_rgb(color);
   // place pixel at (x,y)
   fb[y*sinfo.xres+x]=rgbnum;
 }
 
+void art00(struct fb_var_screeninfo info,uint32_t *buf)
+{
+  // genart code begins
+  RGBCOLOR *tronblue=new_color(0x44,0xFF,0xFF);
+  double t=0.0;
+  double r;
+  int cx=1000,cy=400;
+  for(int j=2;j<=20;j++)
+  {
+    for(int i=0;i<360;i++)
+    {
+      t=(PI/180)*i;
+      r=j*10;
+      double a=r+10*cos(t);
+      double b=r+10*sin(t);
+      double xx=cx+round(a*cos(t));
+      double yy=cy-round(b*sin(t));
+      dot(info,buf,xx,yy,*tronblue);
+    }
+    t=0.0;
+  }
+}
+
 // flow begins
-int main(){
+int main()
+{
   const char *fbPath=getenv("FRAMEBUFFER");
   if(!fbPath) fbPath="/dev/fb0";
 
@@ -70,21 +98,6 @@ int main(){
   uint32_t *buf=mmap(NULL,size,PROT_READ|PROT_WRITE,MAP_SHARED,fb,0);
   if(buf==MAP_FAILED) err(EX_IOERR,"%s",fbPath);
 
-  // genart code begins
-  RGBCOLOR *yellow=new_color(0x44,0xFF,0xFF);
-  double t=0.0;
-  double r;
-  int cx=1000,cy=400;
-  for(int j=2;j<=20;j++){
-    for(int i=0;i<360;i++){
-      t=(PI/180)*i;
-      r=j*10;
-      double a=r+10*cos(t);
-      double b=r+10*sin(t);
-      double xx=cx+round(a*cos(t));
-      double yy=cy-round(b*sin(t));
-      dot(info,buf,xx,yy,*yellow);
-    }
-    t=0.0;
-  }
+  // genart function
+  art00(info,buf);
 }
